4d7c09d786ffd24f74edbca00db467d25f39491a
jongwanra <najongsi@gmail.com>
2020-03-17 14:51:41 +0900

    Initial Commit

---
+++ C_language/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_language
+//
+//  Created by JW on 2020/03/17.
+//  Copyright © 2020 JW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
