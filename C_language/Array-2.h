c23d2974ce4bcff4cb663c424cb2b1398961f1ba
jongwanra <najongsi@gmail.com>
2020-04-28 14:48:50 +0900

    2020.04.28, add Array Problem

---
+++ C_language/Array-2.h
@@ -0,0 +1,14 @@
+//
+//  Array-2.h
+//  C_language
+//
+//  Created by JW on 2020/04/28.
+//  Copyright © 2020 JW. All rights reserved.
+//
+
+#ifndef Array_2_h
+#define Array_2_h
+
+#include <stdio.h>
+
+#endif /* Array_2_h */
